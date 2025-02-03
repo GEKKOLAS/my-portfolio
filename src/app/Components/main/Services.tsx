3696b42448849f82450987caa2a267e1b851c985
Gekkolas <nikolasarango@outlook.com>
2025-02-03 01:11:23 -0500

    [Add] Text for services

--- src/app/Components/main/Services.tsx
+++ src/app/Components/main/Services.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div className="max-w-5xl mx-auto px-8">
+    <div className="max-w-8xl mx-auto px-8">
       <HoverEffect items={services} />
     </div>
   )
@@ -12,39 +12,40 @@ export default Services
 
 export const services = [
     {
-      title: "Stripe",
+      
+      title: "Web Development",
       description:
-        "A technology company that builds economic infrastructure for the internet.",
+        "Designing, developing, and maintaining web applications using ASP.NET, HTML, CSS, JavaScript, and frameworks like Angular or React.",
       link: "https://stripe.com",
     },
     {
-      title: "Netflix",
+      title: "API Development",
       description:
-        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+        "Building and integrating RESTful and GraphQL APIs to facilitate communication between different software systems.",
       link: "https://netflix.com",
     },
     {
-      title: "Google",
+      title: "Database Management",
       description:
-        "A multinational technology company that specializes in Internet-related services and products.",
+        "Designing and managing databases using SQL Server, MySQL, or NoSQL databases like MongoDB, including writing queries, stored procedures, and optimizing performance.",
       link: "https://google.com",
     },
     {
-      title: "Meta",
+      title: "Cloud Services",
       description:
-        "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+        "Deploying and managing applications on cloud platforms like Microsoft Azure, AWS, or Google Cloud, including utilizing cloud services for storage, computing, and DevOps.",
       link: "https://meta.com",
     },
     {
-      title: "Amazon",
+      title: "Mobile App Development",
       description:
-        "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+        "Creating cross-platform mobile applications using Xamarin or .NET MAUI, ensuring smooth performance and a great user experience on both Android and iOS devices.",
       link: "https://amazon.com",
     },
     {
-      title: "Microsoft",
+      title: "DevOps and Continuous Integration/Continuous Deployment (CI/CD)",
       description:
-        "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+        "Implementing CI/CD pipelines using tools like Azure DevOps, Jenkins, or GitHub Actions to automate testing, building, and deploying applications, ensuring faster and more reliable releases.",
       link: "https://microsoft.com",
     },
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "./Components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
+  title: "Portfolio",
   description: "This is my portfolio",
 };
 
